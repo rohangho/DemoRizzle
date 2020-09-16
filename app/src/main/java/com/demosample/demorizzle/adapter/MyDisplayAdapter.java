2d0c431caf40571031f1dbbdedc11e56e352e13e
Rohan Ghosh <rohangho@live.com>
2020-09-16 23:31:26 +0530

    Added the progressbar

--- app/src/main/java/com/demosample/demorizzle/adapter/MyDisplayAdapter.java
+++ app/src/main/java/com/demosample/demorizzle/adapter/MyDisplayAdapter.java
@@ -2,17 +2,24 @@ package com.demosample.demorizzle.adapter;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.demosample.demorizzle.R;
 import com.demosample.demorizzle.model.DisplayModel;
 import com.demosample.demorizzle.model.NodeModel;
@@ -39,11 +46,23 @@ public class MyDisplayAdapter extends RecyclerView.Adapter<MyDisplayAdapter.MyVi
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyDisplayAdapter.MyViewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull final MyDisplayAdapter.MyViewHolder holder, final int position) {
         holder.allCOntent.setText(allList.get(position).getName());
         Glide
                 .with(context)
-                .load(allList.get(position).getImg())
+                .load(allList.get(position).getImg()).addListener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                holder.progressBar.setVisibility(View.GONE);
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                holder.progressBar.setVisibility(View.GONE);
+                return false;
+            }
+        })
                 .error(R.drawable.ic_no_connection)
                 .thumbnail(.1f)
                 .centerCrop()
@@ -61,11 +80,13 @@ public class MyDisplayAdapter extends RecyclerView.Adapter<MyDisplayAdapter.MyVi
 
         private TextView allCOntent;
         private ImageView img;
+        private ProgressBar progressBar;
 
         public MyViewHolder(@NonNull View itemView, Context context) {
             super(itemView);
             allCOntent = itemView.findViewById(R.id.demoText);
             img = itemView.findViewById(R.id.image);
+            progressBar = itemView.findViewById(R.id.progress);
             itemView.setOnClickListener(this);
         }
 
